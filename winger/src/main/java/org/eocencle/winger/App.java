8b0eca0814cfc9d8c9c19701da2cb9e77806e03f
huanStephen <huanStephen@163.com>
2017-10-30 16:45:28 +0800

    winger项目初创

---
+++ winger/src/main/java/org/eocencle/winger/App.java
@@ -0,0 +1,13 @@
+package org.eocencle.winger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
