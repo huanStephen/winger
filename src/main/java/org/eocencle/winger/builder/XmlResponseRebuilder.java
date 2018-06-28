86d73b3fc1fc9eec2f3d8e3ace8a82c8e4584863
huanStephen <huanStephen@163.com>
2018-06-28 22:04:07 +0800

    xml response重载器开发

--- src/main/java/org/eocencle/winger/builder/XmlResponseRebuilder.java
+++ src/main/java/org/eocencle/winger/builder/XmlResponseRebuilder.java
@@ -1,5 +1,9 @@
 package org.eocencle.winger.builder;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
 import org.eocencle.winger.session.Configuration;
 
 /**
@@ -13,6 +17,10 @@ public class XmlResponseRebuilder extends AbstractBuilder {
 	
 	private String updateMode;
 	
+	private Set<String> filePathes = new HashSet<String>();
+	
+	private Integer delay = 0;
+	
 	public XmlResponseRebuilder(Configuration config, XmlResponseBuilder builder, String updateMode) {
 		super(config);
 		this.builder = builder;
@@ -21,8 +29,33 @@ public class XmlResponseRebuilder extends AbstractBuilder {
 
 	@Override
 	public Configuration parse() {
+		for (String resource : this.filePathes) {
+			if (this.config.xmlRespFileContains(resource)) {
+				this.builder.loadResponse(resource);
+			}
+		}
 		
-		return null;
+		this.config.getResponseCache().clear();
+		this.filePathes.clear();
+		return this.config;
+	}
+	
+	public void addFilePath(String filePath) {
+		if (StringUtils.isNoneBlank(filePath)) {
+			this.filePathes.add(filePath);
+		}
+	}
+	
+	public void addFilePath(Set<String> filePathes) {
+		this.filePathes.addAll(filePathes);
+	}
+
+	public Integer getDelay() {
+		return delay;
+	}
+
+	public void setDelay(Integer delay) {
+		this.delay = delay;
 	}
 
 }
